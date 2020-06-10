5c11740acca509dbd88c8e62d78119665b74f01e
Daniel Dai <daeming@gmail.com>
2020-06-10 21:54:42 +0800

    重构代码

---
+++ lib/base.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class IconMeta {
+  final String name;
+  final IconData icon;
+
+  const IconMeta({this.name, this.icon});
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'material_icons.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -16,43 +18,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class IconMeta {
-  final String name;
-  final IconData icon;
-
-  const IconMeta({this.name, this.icon});
-}
-
-const ICONS = [
-  IconMeta(name: 'favorite', icon: Icons.favorite),
-  IconMeta(name: 'threed_rotation', icon: Icons.threed_rotation),
-  IconMeta(name: 'four_k', icon: Icons.four_k),
-  IconMeta(name: 'ac_unit', icon: Icons.ac_unit),
-  IconMeta(name: 'access_alarm', icon: Icons.access_alarm),
-  IconMeta(name: 'access_alarms', icon: Icons.access_alarms),
-  IconMeta(name: 'access_time', icon: Icons.access_time),
-  IconMeta(name: 'accessibility', icon: Icons.accessibility),
-  IconMeta(name: 'accessibility_new', icon: Icons.accessibility_new),
-  IconMeta(name: 'accessible', icon: Icons.accessible),
-  IconMeta(name: 'accessible_forward', icon: Icons.accessible_forward),
-  IconMeta(name: 'account_balance', icon: Icons.account_balance),
-  IconMeta(name: 'account_balance_wallet', icon: Icons.account_balance_wallet),
-  IconMeta(name: 'account_box', icon: Icons.account_box),
-  IconMeta(name: 'account_circle', icon: Icons.account_circle),
-
-  IconMeta(name: 'adb', icon: Icons.adb),
-  IconMeta(name: 'add', icon: Icons.add),
-  IconMeta(name: 'add_a_photo', icon: Icons.add_a_photo),
-  IconMeta(name: 'add_alarm', icon: Icons.add_alarm),
-  IconMeta(name: 'add_alert', icon: Icons.add_alert),
-  IconMeta(name: 'add_box', icon: Icons.add_box),
-  IconMeta(name: 'add_call', icon: Icons.add_call),
-  IconMeta(name: 'add_circle', icon: Icons.add_circle),
-  IconMeta(name: 'add_circle_outline', icon: Icons.add_circle_outline),
-  IconMeta(name: 'add_comment', icon: Icons.add_comment),
-  IconMeta(name: 'add_location', icon: Icons.add_location),
-  IconMeta(name: 'add_photo_alternate', icon: Icons.add_photo_alternate),
-];
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -100,7 +65,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: ListView(children: ICONS.map((icon) {
+      body: ListView(children: MATERIAL_ICONS.map((icon) {
         return ListTile(
           leading: Icon(icon.icon),
           title: Text(icon.name),
